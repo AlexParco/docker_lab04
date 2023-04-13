54a1f3b6e2886fa97c6f136a3598daf9fcc8fd15
AlexParco <alexparco16@gmail.com>
2023-04-13 10:07:21 -0500

    lab 04

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const bodyParse = require('body-parser')
+const app = express()
+const cors = require('cors')
+const PORT = process.env.PORT || 5000
+
+const productos = [
+    {id: 1, nombre: 'Manzana'},
+    {id: 2, nombre: 'Pera'},
+    {id: 3, nombre: 'Papaya'}
+]
+
+const clientes = [
+    {id: 1, nombre: 'Alexander Parco', productos: [productos[0], productos[1]]},
+    {id: 2, nombre: 'Alonso Parco', productos: [productos[2], productos[1]]},
+    {id: 3, nombre: 'Jose Palomino', productos: [productos[1], productos[0]]}
+]
+
+
+app.use(bodyParse.json())
+app.use(bodyParse.urlencoded({
+  extended: true
+}))
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hola Soy Alexander Parco!')
+})
+
+app.get('/clientes', (req, res) => {
+    res.status(200).json(clientes)
+})
+
+app.get('/productos', (req,res) => {
+    res.status(200).json(productos)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
+})
